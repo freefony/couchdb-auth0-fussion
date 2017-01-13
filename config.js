f730ed85e9f2b5fb08e3e9585045f99aedc11fac
Femi Oni <freefony@gmail.com>
2017-01-13 23:16:03 +0100

    add http module

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const admin_username = process.env.uname
 const admin_password = process.env.pass
 
-module.exports.config = {
+module.exports = {
   dev: {
-    CouchDB_URL: 'http://admin_username:admin_password@localhost:5984/_users'
+    CouchDB_URL: 'http://' + admin_username + ':' + admin_password + '@localhost:5984/_users'
   }
 }
