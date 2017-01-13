6c441f11a011958bbbe4de58aac76f40e55eed6a
Femi Oni <freefony@gmail.com>
2017-01-13 12:44:41 +0100

    improve commit with env variable to login to db

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 //const/let
+const admin_username = process.env.uname
+const admin_password = process.env.pass
 
 module.exports.config = {
   dev: {
-    CouchDB_URL: 'http://localhost:5984/_users'
+    CouchDB_URL: 'http://admin_username:admin_password@localhost:5984/_users'
   }
 }
